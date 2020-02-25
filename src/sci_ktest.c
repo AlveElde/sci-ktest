b8c05fdde924f0f4f9c38dc3c830232ec37b7f1f
AlveElde <alve_elde@hotmail.com>
2020-02-25 01:51:11 +0100

    Add create MSQ

--- src/sci_ktest.c
+++ src/sci_ktest.c
@@ -16,13 +16,63 @@ MODULE_LICENSE("GPL");
 #define SCIL_INIT_FLAGS 0
 #define SCIL_EXIT_FLAGS 0
 
+static int create_msq(struct msq_ctx* msq)
+{
+    sci_error_t err;
+    pr_devel(STATUS_START);
 
+    err = SCILCreateMsgQueue(&msq->msq, 
+                                msq->localAdapterNo, 
+                                msq->remoteNodeId, 
+                                msq->lmsqId,
+                                msq->rmsqId, 
+                                msq->maxMsgCount, 
+                                msq->maxMsgSize, 
+                                msq->timeout, 
+                                msq->flags);
+    switch (err)
+    {
+    case SCI_ERR_ILLEGAL_PARAMETER:
+        pr_devel("SCI_ERR_ILLEGAL_PARAMETER: " STATUS_FAIL);
+        return -42;
+    case SCI_ERR_NOSPC:
+        pr_devel("SCI_ERR_NOSPC: " STATUS_FAIL);
+        return -42;
+    default:
+        pr_devel(STATUS_COMPLETE);
+        return 0;
+    }
+}
 
 static void perform_test(void)
 {
+    int ret;
+    struct msq_ctx msq;
 
+    pr_devel(STATUS_START);
+
+    memset(&msq, 0, sizeof(struct msq_ctx));
+    msq.msq             = NULL;
+    msq.localAdapterNo  = 0;
+    msq.remoteNodeId    = 99;
+    msq.lmsqId          = 1;
+    msq.rmsqId          = 2;
+    msq.maxMsgCount     = 16;
+    msq.maxMsgSize      = 128;
+    msq.timeout         = 10;
+    msq.flags           = 0;
+    ret = create_msq(&msq);
+    if(ret) {
+        goto create_msq_err;
+    }
+
+
+    SCILRemoveMsgQueue(&msq.msq, 0);
+
+create_msq_err:
+    pr_devel(STATUS_COMPLETE);
+    return;
 }
-
 
 static int __init sci_ktest_init(void)
 {
--- src/sci_ktest.h
+++ src/sci_ktest.h
@@ -1,3 +1,16 @@
 #define STATUS_START    "Started.\n"
 #define STATUS_COMPLETE "Completed.\n"
 #define STATUS_FAIL     "Failed.\n"
+
+
+struct msq_ctx {
+    sci_msq_queue_t msq;
+    unsigned int    localAdapterNo; 
+    unsigned int    remoteNodeId;   
+    unsigned int    lmsqId;
+    unsigned int    rmsqId;
+    unsigned int    maxMsgCount;    
+    unsigned int    maxMsgSize;  
+    unsigned int    timeout;
+    unsigned int    flags;
+};
